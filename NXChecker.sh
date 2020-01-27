2732bb098c5805d4bbc55797a6f89d09615b8000
Kenny Van <k5van@ucsd.edu>
2020-01-27 00:37:51 -0800

    Updated quarter

---
+++ NXChecker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./nxcacheCheck.sh new-royal-male-hairs 2800 100
+#./nxcacheCheck.sh pssb-1 2800 100
+#./nxcacheCheck.sh pssb-2 2800 100
+#./nxcacheCheck.sh pssb-3 2800 100
+#./nxcacheCheck.sh pssb-4 2800 100
+./nxcacheCheck.sh new-royal-female-hairs 2800 100
+./nxcacheCheck.sh new-royal-female-faces 2800 100
+./nxcacheCheck.sh new-royal-male-faces 2800 100
+./nxcacheCheck.sh royal-male-hairs 2800 100
+./nxcacheCheck.sh royal-male-faces 2800 100
+./nxcacheCheck.sh royal-female-hairs 2800 100
+./nxcacheCheck.sh royal-female-faces 2800 100
